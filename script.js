f21a007809a79f61390ae02ca330e6cac159feba
dreessen-n <dreessen.edu@gmail.com>
2022-05-14 14:09:15 -0600

    Add click events to login btn and subscribe btn

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 console.log("page loaded...");
 
-function playVideo(name) {
-    var x = document.getElementById(name);
+function playVideo(idName) {
+    var x = document.getElementById(idName);
     x.play();
 }
 
-function pauseVideo(name) {
-    var x = document.getElementById(name);
+function pauseVideo(idName) {
+    var x = document.getElementById(idName);
     x.pause();
+}
+function signInOut(idName) {
+    var text = document.getElementById(idName).innerText;
+    if (text == 'Login') {
+        document.getElementById(idName).innerText = 'Logged Out';
+    } else {
+        document.getElementById(idName).innerText = 'Login'
+    }
+}
+
+function subscribe(idName) {
+    var text = document.getElementById(idName).innerText;
+    if (text == 'Subscribe') {
+        document.getElementById(idName).innerText = 'Subscribed';
+    } else {
+        document.getElementById(idName).innerText = 'Subscribe'
+    }
 }
