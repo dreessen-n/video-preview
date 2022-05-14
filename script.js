af51f653b0ba1858acd77a868ad5c5f7c0cd07c0
dreessen-n <dreessen.edu@gmail.com>
2022-05-13 21:08:52 -0600

    Add onmouseover playVideo() and onmouseleave pauseVideo()

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("page loaded...");
+
+function playVideo() {
+    var x = document.getElementById('swissVideo');
+    x.play();
+}
+
+function pauseVideo() {
+    var x = document.getElementById('swissVideo');
+    x.pause();
+}
